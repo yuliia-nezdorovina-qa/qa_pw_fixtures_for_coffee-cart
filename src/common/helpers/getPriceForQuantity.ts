9672d53a158f7c5d7e3be5a8b00201a960e8789f
Yuliia Nezdorovina <yuliia.aqa@gmail.com>
2026-09-07 13:58:32 +0300

    Fix round 1 after suggestions from code review

--- src/common/helpers/getPriceForQuantity.ts
+++ src/common/helpers/getPriceForQuantity.ts
@@ -1,17 +1,11 @@
-export async function unitPriceFormatStr(
-  unitsNumber: number,
-  unitPrice: number,
-) {
+export function unitPriceFormatStr(unitsNumber: number, unitPrice: number) {
   return `${unitPrice.toFixed(2)} x ${unitsNumber}`;
 }
 
-export async function priceFormatStr(unitPrice: number) {
+export function priceFormatStr(unitPrice: number) {
   return `$${unitPrice.toFixed(2)}`;
 }
 
-export async function totalPriceFormatStr(
-  unitsNumber: number,
-  unitPrice: number,
-) {
+export function totalPriceFormatStr(unitsNumber: number, unitPrice: number) {
   return `Total: $${(unitsNumber * unitPrice).toFixed(2)}`;
 }
